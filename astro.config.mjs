7bfac24f8ea1e460bb2013bb87766fa14c09b0c7
sirlilpanda <sirlilpanda@gmail.com>
2024-12-02 14:58:03 +1300

    added remark and katex to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import rehypeKatex from 'rehype-katex'; // relevant
+import remarkMath from 'remark-math';   // relevant
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://cansource.org',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx({
+			remarkPlugins: [remarkMath], // relevant
+			rehypePlugins: [rehypeKatex] // relevant
+		}), 
+		sitemap()
+	],
 });
